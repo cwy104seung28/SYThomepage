e5f16e020383a235d166a53f88fe5d84a6df63f0
cwy阿瑜 <68813383+cwy104seung28@users.noreply.github.com>
2022-05-11 18:16:35 +0800

    0511 update

--- assests/js/index.js
+++ assests/js/index.js
@@ -7,12 +7,12 @@
 //   });
 // });
 // function color_change() {
-//     var footer_top = $('footer').offset().top;
-//     var footer_height = $('footer').height();
-//     var footer;
-//     if (window.innerWidth > 1374) {
-//         footer = footer_top + footer_height * 0.55;
-//     }
+//     var nav_top = $('nav').offset().top;
+//     // var footer_height = $('footer').height();
+//     // var footer;
+//     // if (window.innerWidth > 1374) {
+//     //     footer = footer_top + footer_height * 0.55;
+//     // }
 //     // else if (window.innerWidth <= 1374 && window.innerWidth > 1120) {
 //     //     footer = footer_top + footer_height * 0.4;
 //     // }
@@ -22,8 +22,9 @@
 //     // else{
 //     //     footer = footer_top + footer_height * 1;
 //     // }
+//     // console.log();
 //     $('.change_color').each(function () {
-//         if ($(this).offset().top >= footer) {
+//         if ($(this).offset().top >= nav_top) {
 //             $(this).css("color","#FFFFFF")
 //         }
 //         else {
@@ -31,21 +32,57 @@
 //                 var color = $(this).attr('data-color');
 //             }
 //             else {
-//                 var color = "#000000";
+//                 var color = "#999999";
 //             }
 //             $(this).css("color", color);
 //         }
 //     });
 
 // }
-document.getElementsByClassName("main")[0].οnscrοll = function (e) {
-  console.log(e.target.scrollTop);
-  //当滚动出去的内容高度超出100，实现变色效果
-  if (e.target.scrollTop >= 100) {
-    document.getElementsByClassName("nav-container")[0].style.background =
-      "#999999";
-  } else {
-    document.getElementsByClassName("nav-container")[0].style.background =
-      "#FFFFFF";
+// document.getElementsByClassName("main")[0].οnscrοll = function (e) {
+//   console.log(e.target.scrollTop);
+//   //当滚动出去的内容高度超出100，实现变色效果
+//   if (e.target.scrollTop >= 100) {
+//     document.getElementsByClassName("nav-container")[0].style.background =
+//       "#999999";
+//   } else {
+//     document.getElementsByClassName("nav-container")[0].style.background =
+//       "#FFFFFF";
+//   }
+// };
+
+// 滑動到最上方的效果---------------
+$(window).scroll(function () {
+    // if ($(window).width() > 768) {
+    //   // if ($(window).scrollTop() <= 0) {
+    //   //   $(".nav,.en,.ch,.explore,.line").addClass("top");
+    //   // }
+    //   // else {
+    //   //   $(".nav,.en,.ch,.explore,.line").removeClass("top");
+    //   // }
+    // }
+  
+    setScroll();
+  
+  });
+  
+  // 滑動出現show效果---------------
+  function setScroll() {
+    var win = $(window),
+      hWin = win.height(),
+      wWin = win.width(),
+      scroll = $(window).scrollTop();
+    var move = $(".bigTitle2,.appSystem,.mSystem,.computer,.idealpart");
+  
+    move.each(function () {
+      var tCont = $(this).offset().top;
+      if (scroll > tCont - hWin * 0.8) {
+        $(this).addClass("show");
+      }
+      else {
+        $(this).removeClass("show");
+      }
+  
+    })
   }
-};
+  
