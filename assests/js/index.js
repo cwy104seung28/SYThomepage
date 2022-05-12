f3335cd0417e2565095a23a505b7b11f96b1c7c2
cwy阿瑜 <68813383+cwy104seung28@users.noreply.github.com>
2022-05-12 13:38:25 +0800

    0512 navbar往下滑可變色

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,56 +1,3 @@
-// $(document).ready(function () {
-//   window.onresize = function () {
-//     color_change();
-//   };
-//   window.addEventListener("scroll", function () {
-//     color_change();
-//   });
-// });
-// function color_change() {
-//     var nav_top = $('nav').offset().top;
-//     // var footer_height = $('footer').height();
-//     // var footer;
-//     // if (window.innerWidth > 1374) {
-//     //     footer = footer_top + footer_height * 0.55;
-//     // }
-//     // else if (window.innerWidth <= 1374 && window.innerWidth > 1120) {
-//     //     footer = footer_top + footer_height * 0.4;
-//     // }
-//     // else if (window.innerWidth <= 1120 && window.innerWidth > 768) {
-//     //     footer = footer_top + footer_height * 0.35;
-//     // }
-//     // else{
-//     //     footer = footer_top + footer_height * 1;
-//     // }
-//     // console.log();
-//     $('.change_color').each(function () {
-//         if ($(this).offset().top >= nav_top) {
-//             $(this).css("color","#FFFFFF")
-//         }
-//         else {
-//             if ($(this).attr('data-color')) {
-//                 var color = $(this).attr('data-color');
-//             }
-//             else {
-//                 var color = "#999999";
-//             }
-//             $(this).css("color", color);
-//         }
-//     });
-
-// }
-// document.getElementsByClassName("main")[0].οnscrοll = function (e) {
-//   console.log(e.target.scrollTop);
-//   //当滚动出去的内容高度超出100，实现变色效果
-//   if (e.target.scrollTop >= 100) {
-//     document.getElementsByClassName("nav-container")[0].style.background =
-//       "#999999";
-//   } else {
-//     document.getElementsByClassName("nav-container")[0].style.background =
-//       "#FFFFFF";
-//   }
-// };
-
 // 滑動到最上方的效果---------------
 $(window).scroll(function () {
   // if ($(window).width() > 768) {
@@ -72,7 +19,6 @@ function setScroll() {
     wWin = win.width(),
     scroll = $(window).scrollTop();
   var move = $("#service,#about,#contact");
-  var animation = $("#card-1,#card-2,#card-3");
 
   move.each(function () {
     var tCont = $(this).offset().top;
@@ -82,9 +28,10 @@ function setScroll() {
         for (var i = 1; i < 4; i++) {
           $(`#card-${i}`).addClass(`drop-animation-${i}`);
         }
-        // $("#card-1").addClass("drop-animation-1");
-        // $("#card-2").addClass("drop-animation-2");
-        // $("#card-3").addClass("drop-animation-3");
+      }
+      if ($("#contact").hasClass("show")) {
+        $(".top-btn").removeClass("not-show-btn");
+        $(".top-btn").addClass("show-btn");
       }
     } else {
       $(this).removeClass("show");
@@ -93,16 +40,17 @@ function setScroll() {
           $(`#card-${i}`).removeClass(`drop-animation-${i}`);
         }
       }
+      if (!$("#contact").hasClass("show")) {
+        $(".top-btn").removeClass("show-btn");
+        $(".top-btn").addClass("not-show-btn");
+      }
     }
   });
-  // animation.each(function () {
-  //   var tCont = $(this).offset().top;
-  //   if (scroll > tCont - hWin * 1.1) {
-  //     $(this).addClass("drop-animation");
-  //   }
-  //   else {
-  //     $(this).removeClass("drop-animation");
-  //   }
-
-  // })
+  var tCont = $("#banner").offset().top;
+  if (scroll > tCont) {
+    $(".nav-container").addClass("change-nav");
+  } else {
+    $(".nav-container").removeClass("change-nav");
+  }
+  console.log($("#about").offset().top - hWin);
 }
