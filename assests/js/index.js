35cb1b42b2369af7da3d0bd4ed7b001b4d76c5ca
cwy阿瑜 <68813383+cwy104seung28@users.noreply.github.com>
2022-05-12 09:57:30 +0800

    0512 service 增加animation

--- assests/js/index.js
+++ assests/js/index.js
@@ -53,36 +53,56 @@
 
 // 滑動到最上方的效果---------------
 $(window).scroll(function () {
-    // if ($(window).width() > 768) {
-    //   // if ($(window).scrollTop() <= 0) {
-    //   //   $(".nav,.en,.ch,.explore,.line").addClass("top");
-    //   // }
-    //   // else {
-    //   //   $(".nav,.en,.ch,.explore,.line").removeClass("top");
-    //   // }
-    // }
-  
-    setScroll();
-  
+  // if ($(window).width() > 768) {
+  //   // if ($(window).scrollTop() <= 0) {
+  //   //   $(".nav,.en,.ch,.explore,.line").addClass("top");
+  //   // }
+  //   // else {
+  //   //   $(".nav,.en,.ch,.explore,.line").removeClass("top");
+  //   // }
+  // }
+
+  setScroll();
+});
+
+// 滑動出現show效果---------------
+function setScroll() {
+  var win = $(window),
+    hWin = win.height(),
+    wWin = win.width(),
+    scroll = $(window).scrollTop();
+  var move = $("#service,#about,#contact");
+  var animation = $("#card-1,#card-2,#card-3");
+
+  move.each(function () {
+    var tCont = $(this).offset().top;
+    if (scroll > tCont - hWin * 0.8) {
+      $(this).addClass("show");
+      if ($("#service").hasClass("show")) {
+        for (var i = 1; i < 4; i++) {
+          $(`#card-${i}`).addClass(`drop-animation-${i}`);
+        }
+        // $("#card-1").addClass("drop-animation-1");
+        // $("#card-2").addClass("drop-animation-2");
+        // $("#card-3").addClass("drop-animation-3");
+      }
+    } else {
+      $(this).removeClass("show");
+      if (!$("#service").hasClass("show")) {
+        for (var i = 1; i < 4; i++) {
+          $(`#card-${i}`).removeClass(`drop-animation-${i}`);
+        }
+      }
+    }
   });
-  
-  // 滑動出現show效果---------------
-  function setScroll() {
-    var win = $(window),
-      hWin = win.height(),
-      wWin = win.width(),
-      scroll = $(window).scrollTop();
-    var move = $(".bigTitle2,.appSystem,.mSystem,.computer,.idealpart");
-  
-    move.each(function () {
-      var tCont = $(this).offset().top;
-      if (scroll > tCont - hWin * 0.8) {
-        $(this).addClass("show");
-      }
-      else {
-        $(this).removeClass("show");
-      }
-  
-    })
-  }
-  
+  // animation.each(function () {
+  //   var tCont = $(this).offset().top;
+  //   if (scroll > tCont - hWin * 1.1) {
+  //     $(this).addClass("drop-animation");
+  //   }
+  //   else {
+  //     $(this).removeClass("drop-animation");
+  //   }
+
+  // })
+}
