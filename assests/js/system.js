5f7df82ef1be3cd73925b8181fd926010a4f94b6
cwyADT107102 <68813383+cwy104seung28@users.noreply.github.com>
2022-05-26 18:18:47 +0800

    0526 update

--- assests/js/system.js
+++ assests/js/system.js
@@ -6,6 +6,34 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+const asideArr=[
+  '公文-收文管理',
+  '公文-發文管理',
+  '公文-基本設定',
+  '檔案管理',
+  '會議管理',
+  '事項管理',
+  '行事曆',
+  '簽核管理'
+]
+
+var paginationSpan = [document.getElementsByClassName("swiper-pagination-bullet")];
+
+var i;
+for (i = 0; i < paginationSpan.length; i++) {
+    var span = document.createElement('span');
+    span.textContent = asideArr[i];
+    document.querySelector('.swiper-pagination-bullet').appendChild(span);
+}
+// paginationSpan.forEach(element => {
+//   var i=0;
+//   element.textContent = asideArr[i];
+//   i++;
+//   console.log(asideArr[i]);
+// });
+
+
+
 $(window).scroll(function () {
   setScroll();
 });
