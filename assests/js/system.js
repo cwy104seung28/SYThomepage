271a063577f8c752d7a926c9545cec236b6af94d
cwyADT107102 <68813383+cwy104seung28@users.noreply.github.com>
2022-05-27 17:52:15 +0800

    0527 手機板swiper調整

--- assests/js/system.js
+++ assests/js/system.js
@@ -30,23 +30,75 @@ var paginationSpan = document.getElementsByClassName("swiper-pagination-bullet")
 var i;
 for (i = 0; i < asideArr.length; i++) {
 
-    // var spanBefore = document.createElement('span::before');
-    // spanBefore.style['content']='';
-    // spanBefore.style['width']='10px';
-    // spanBefore.style['height']='40px';
-    // spanBefore.style['background-color']='#007aff';
-
     var span = document.createElement('span');
     span.textContent = asideArr[i];
 
     paginationSpan[i].appendChild(span);
 }
-// paginationSpan.forEach(element => {
-//   var i=0;
-//   element.textContent = asideArr[i];
-//   i++;
-//   console.log(asideArr[i]);
-// });
+$(document).ready(function () {
+  var information_H_1 = $('#information-1').height();
+  var information_H_2 = $('#information-2').height();
+
+  document.onreadystatechange = function () {
+      if (document.readyState == "complete") {
+          if ( window.innerWidth > 767){
+              if(information_H_1 < 258){
+                  $('#about-shop-1 .about-shop-menu').addClass('no_visible');
+              }
+              else{
+                  $('#about-shop-1 .about-shop-menu').removeClass('no_visible');
+      
+              };
+
+              if(information_H_2 < 258){
+                  $('#about-shop-2 .about-shop-menu').addClass('no_visible');
+              }
+              else{
+                  $('#about-shop-2 .about-shop-menu').removeClass('no_visible');
+      
+              };
+          }
+          else if( window.innerWidth > 767){
+              if(information_H_1 < 74){
+                  $('#about-shop-1 .about-shop-menu').addClass('no_visible');
+              }
+              else{
+                  $('#about-shop-1 .about-shop-menu').removeClass('no_visible');
+      
+              };
+
+              if(information_H_2 < 74){
+                  $('#about-shop-2 .about-shop-menu').addClass('no_visible');
+              }
+              else{
+                  $('#about-shop-2 .about-shop-menu').removeClass('no_visible');
+              };
+          }
+          if (window.innerWidth < 1753 && window.innerWidth > 767) {
+              $(".nav-slider-container .swiper-wrapper .swiper-slide").width(window.innerWidth * 0.674);
+              $(".nav-slider-container .swiper-wrapper .swiper-slide").height(window.innerWidth * 0.284);
+              swiper.update();
+          }
+          else {
+              $(".nav-slider-container .swiper-wrapper .swiper-slide").width("");
+              $(".nav-slider-container .swiper-wrapper .swiper-slide").height("");
+          }
+          setTimeout(function () { swiper.update(); }, 300); 
+      }
+  };
+  window.onresize = function () {
+      if ( window.innerWidth < 767) {
+          $(".nav-slider-container .swiper-wrapper .swiper-slide").width(window.innerWidth * 0.674);
+          $(".nav-slider-container .swiper-wrapper .swiper-slide").height(window.innerWidth * 0.284);
+          swiper.update();
+      }
+      else {
+          $(".nav-slider-container .swiper-wrapper .swiper-slide").width(window.innerWidth);
+          $(".nav-slider-container .swiper-wrapper .swiper-slide").height(window.innerWidth);
+      }
+      setTimeout(function () { swiper.update(); }, 300);  
+  };
+});
 
 
 
